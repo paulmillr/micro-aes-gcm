d21d956d37109dbe50e538354af2be425f93d97c
Paul Miller <paul@paulmillr.com>
2020-05-04 00:53:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,50 +1,23 @@
-/*! noble-secretbox-aes-gcm - MIT License (c) Paul Miller (paulmillr.com) */
 "use strict";
 
-const MODE = "aes-256-gcm";
-const MODE_B = "AES-GCM";
-const MODE_PARAMS = { name: MODE_B, length: 256 };
+const AES_MODE_N = "aes-256-gcm";
+const AES_MODE_B = "AES-GCM";
+const AES_MODE_BB = { name: AES_MODE_B, length: 256 };
 
-const isBrowser = typeof window == "object" && "crypto" in window;
-const cryp = !isBrowser && require("crypto");
+const isBrowser = typeof window !== 'undefined' && window.crypto;
 const wcryp = isBrowser && window.crypto;
-let secureRandom = bytesLength => new Uint8Array(bytesLength);
+const cryp = !isBrowser && require("crypto");
+const secureRandom = wcryp ?
+  (length => window.crypto.getRandomValues(new Uint8Array(length))) :
+  (length => new Uint8Array(randomBytes(length).buffer));
 
-const Encoder =
-  !isBrowser && typeof TextEncoder === "undefined"
-    ? require("util").TextEncoder
-    : TextEncoder;
-const Decoder =
-  !isBrowser && typeof TextDecoder === "undefined"
-    ? require("util").TextDecoder
-    : TextDecoder;
-
-if (isBrowser) {
-  secureRandom = bytesLength => {
-    const array = new Uint8Array(bytesLength);
-    wcryp.getRandomValues(array);
-    return array;
-  };
-} else if (typeof process === "object" && "node" in process.versions) {
-  secureRandom = bytesLength => {
-    const b = cryp.randomBytes(bytesLength);
-    return new Uint8Array(b.buffer, b.byteOffset, b.byteLength);
-  };
-} else {
-  throw new Error(
-    "The environment doesn't have cryptographically secure random function"
-  );
-}
-// function ui8aToHex(ui8a) {
-//   return Array.from(ui8a)
-//     .map(c => c.toString(16).padStart(2, "0"))
-//     .join("");
-// }
-function hexToUi8a(hex) {
-  if (hex.length % 2 !== 0) throw new RangeError("hex length is invalid");
+function hexToArray(hex) {
+  if (hex.startsWith('0x')) hex = hex.slice(2);
+  if (hex.length & 1) hex = `0${hex}`;
   const array = new Uint8Array(hex.length / 2);
-  for (let i = 0; i < hex.length; i += 2) {
-    array[i / 2] = parseInt(hex.substr(i, 2), 16);
+  for (let i = 0; i < array.length; i++) {
+    let j = i * 2;
+    array[i] = Number.parseInt(hex.slice(j, j + 2), 16);
   }
   return array;
 }
@@ -56,18 +29,18 @@ function hexToUi8a(hex) {
  */
 async function encrypt(sharedKey, plaintext) {
   if (typeof plaintext === "string")
-    plaintext = new Encoder().encode(plaintext);
+    plaintext = new TextEncoder().encode(plaintext);
   const iv = secureRandom(12);
   if (isBrowser) {
     const bSharedKey = await wcryp.subtle.importKey(
       "raw",
       sharedKey,
-      MODE_PARAMS,
+      AES_MODE_BB,
       true,
       ["encrypt"]
     );
     const cipher = await wcryp.subtle.encrypt(
-      { name: MODE_B, iv },
+      { name: AES_MODE_B, iv },
       bSharedKey,
       plaintext
     );
@@ -77,10 +50,10 @@ async function encrypt(sharedKey, plaintext) {
     encrypted.set(ciphertext, iv.length);
     return encrypted;
   } else {
-    const cipher = cryp.createCipheriv(MODE, sharedKey, iv);
+    const cipher = cryp.createCipheriv(AES_MODE_N, sharedKey, iv);
     let ciphertext = cipher.update(plaintext, undefined, "hex");
     ciphertext += cipher.final("hex");
-    const ciphertextArray = hexToUi8a(ciphertext);
+    const ciphertextArray = hexToArray(ciphertext);
     const tag = cipher.getAuthTag();
     const encrypted = new Uint8Array(
       iv.length + ciphertextArray.length + tag.length
@@ -98,19 +71,19 @@ async function encrypt(sharedKey, plaintext) {
  * @returns {Promise<Uint8Array>}
  */
 async function decrypt(sharedKey, encoded) {
-  if (typeof encoded === "string") encoded = hexToUi8a(encoded);
+  if (typeof encoded === "string") encoded = hexToArray(encoded);
   const iv = encoded.slice(0, 12);
   if (isBrowser) {
     const ciphertextWithTag = encoded.slice(12);
     const bSharedKey = await wcryp.subtle.importKey(
       "raw",
       sharedKey,
-      MODE_PARAMS,
+      AES_MODE_BB,
       true,
       ["decrypt"]
     );
     const plaintext = await wcryp.subtle.decrypt(
-      { name: MODE_B, iv },
+      { name: AES_MODE_B, iv },
       bSharedKey,
       ciphertextWithTag
     );
@@ -118,7 +91,7 @@ async function decrypt(sharedKey, encoded) {
   } else {
     const ciphertext = encoded.slice(12, -16);
     const authTag = encoded.slice(-16);
-    const decipher = cryp.createDecipheriv(MODE, sharedKey, iv);
+    const decipher = cryp.createDecipheriv(AES_MODE_N, sharedKey, iv);
     decipher.setAuthTag(authTag);
     const plaintext = decipher.update(ciphertext);
     const res = Buffer.concat([plaintext, decipher.final()]);
@@ -131,13 +104,13 @@ async function decrypt(sharedKey, encoded) {
  * @param {Uint8Array} byteArray
  * @returns {string}
  */
-function toUTF8(byteArray) {
-  return new Decoder().decode(byteArray);
+function bytesToUTF8(byteArray) {
+  return new TextDecoder().decode(byteArray);
 }
 
 if (typeof exports !== "undefined") {
   Object.defineProperty(exports, "__esModule", { value: true });
   exports.decrypt = decrypt;
   exports.encrypt = encrypt;
-  exports.toUTF8 = toUTF8;
+  exports.bytesToUTF8 = bytesToUTF8;
 }
